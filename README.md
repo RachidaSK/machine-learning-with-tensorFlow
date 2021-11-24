f9505b72d213b20c013ceba61949d4a0daf4086d
RachidaSK <rachida.koco@gmail.com>
2021-11-23 20:36:42 -0500

    train a model with MNIST dataset~

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # machine-learning-with-tensorFlow
 
+In this project, we trained a machine learning model using a prebuilt dataset (MNIST) using the Keras API.
+
 https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/quickstart/beginner.ipynb#scrollTo=l2hiez2eIUz8
--- tensor.py
+++ tensor.py
@@ -1,6 +1,8 @@
 # set up tensorflow
 import tensorflow as tf
+
 print("TensorFlow version:", tf.__version__)
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 # Load Data
 mnist = tf.keras.datasets.mnist
@@ -19,3 +21,24 @@ model = tf.keras.models.Sequential([
 # For each example, the model returns a vector of logits or log-odds scores, one for each class.
 predictions = model(x_train[:1]).numpy()
 predictions
+
+tf.nn.softmax(predictions).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+probability_model(x_test[:5])
